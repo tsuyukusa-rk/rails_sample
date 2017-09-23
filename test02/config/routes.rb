b9b6c5970cb20d90198685a95afc4f6e79770765
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-23 17:54:45 +0900

    CRUD resourses routes

--- test02/config/routes.rb
+++ test02/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # RESTfulに応じて、CRUDのルーティングを用意してくれる
+  resources :users
+  resources :members do
+    # 複数に対しt
+    collection { get 'search' }
+    # 単数に対して
+    member { patch 'confirm', 'regist' }
+    member { get 'thankyou' }
+  end
 end
