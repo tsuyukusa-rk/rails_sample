f52b0df7bcadbaa25a1d0881b70d74f2ea623a1c
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-23 11:55:31 +0900

    memo

--- test01/config/routes.rb
+++ test01/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # controller + Action名
   # でつなぐ
+  # http_method 'url' => 'controller#method'
+  # rails routes で設定を参照できる
+  # localhost://rails/info/routesでも見れる
   root "top#index"
   get "about" => "top#about"
 end
