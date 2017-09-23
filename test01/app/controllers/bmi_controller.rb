55e5f51ab055471c780d3034901c31363b477976
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-23 15:04:39 +0900

    リファクタ

--- test01/app/controllers/bmi_controller.rb
+++ test01/app/controllers/bmi_controller.rb
@@ -8,25 +8,31 @@ class BmiController < ApplicationController
     @name = params[:name]
     @height = height
     @weight = height
-    calc(height.to_f, weight.to_f)
+    @bmi = calcBmi(height.to_f, weight.to_f)
+    @idealWeight = calcIdealWeight(height.to_f, weight.to_f)
+    @levelOfObesity = calcLevelOfObesity(@bmi)
   end
 
   private
-  def calc (h, w)
-    @bmi = (w / ((h / 100) ** 2)).round
-    @idealWeight = (((h / 100) ** 2) * 22).round
-    if @bmi < 18 then
-      @levelOfObesity = 'レベル-1:低体重'
-    elsif  @bmi < 25 then
-      @levelOfObesity = 'レベル0:普通体重'
-    elsif  @bmi < 30 then
-      @levelOfObesity = 'レベル1:肥満1度'
-    elsif  @bmi < 35 then
-      @levelOfObesity = 'レベル2:肥満2度'
-    elsif  @bmi < 40 then
-      @levelOfObesity = 'レベル3:肥満3度'
+  def calcBmi (h, w)
+    return (w / ((h / 100) ** 2)).round
+  end
+  def calcIdealWeight (h, w)
+    return (((h / 100) ** 2) * 22).round
+  end
+  def calcLevelOfObesity (bmi)
+    if bmi < 18 then
+      return 'レベル-1:低体重'
+    elsif  bmi < 25 then
+      return 'レベル0:普通体重'
+    elsif  bmi < 30 then
+      return 'レベル1:肥満1度'
+    elsif  bmi < 35 then
+      return 'レベル2:肥満2度'
+    elsif  bmi < 40 then
+      return 'レベル3:肥満3度'
     else
-      @levelOfObesity = 'レベル4:肥満4度'
+      return 'レベル4:肥満4度'
     end
   end
 end
