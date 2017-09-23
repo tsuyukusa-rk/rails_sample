24bff730815aa19eb14fcb25723e0e11a503e6b5
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-23 18:20:51 +0900

    CRUDをviewとcontrollerに追加

--- test02/app/controllers/members_controller.rb
+++ test02/app/controllers/members_controller.rb
@@ -2,4 +2,34 @@ class MembersController < ApplicationController
   def index
     @member = Member.find(1)
   end
+  def show
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def new
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def edit
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def create
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def update
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def destroy
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def search
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def confirm
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def regist
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
+  def thankyou
+    render text: '#[params[:controller]] , #[params[:action]], #[params[:id]]'
+  end
 end
