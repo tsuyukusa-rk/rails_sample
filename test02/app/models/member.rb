0a6a1710ac4c7531338a6b4740109cf9f0c1f1a0
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-24 11:25:07 +0900

    add comment

--- test02/app/models/member.rb
+++ test02/app/models/member.rb
@@ -4,6 +4,7 @@ class Member < ApplicationRecord
       rel = order('id')
       # present? は　空じゃないかの判別
       if query.present?
+        # jsのテンプレートリテラルはダブルクォーテーションらしい
         rel = rel.where('name LIKE ?', "%#{query}%")
       end
       return rel
