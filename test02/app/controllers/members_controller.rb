57f0a8cf16cf8ea57437b16ec815d9757448660d
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-09-24 13:32:55 +0900

    アクションコールバックの追加

--- test02/app/controllers/members_controller.rb
+++ test02/app/controllers/members_controller.rb
@@ -1,23 +1,34 @@
 class MembersController < ApplicationController
+  # アクションコールバック
+  # after_action、around_actionとかもある
+  before_action :init
+  private def init
+    @title = '画面'
+  end
   # 一覧表示
   def index
+    @page_title = '一覧' + @title
     # sortさせる
     @members = Member.order("id")
   end
   # 個別ユーザー表示
   def show
+    @page_title = 'ユーザー照会' + @title
     # 1番目を取ってくる
     @member = Member.find(params[:id])
   end
   # 検索結果
   def search
+    @page_title = '検索結果一覧' + @title
     @members = Member.search(params[:q])
     render "index"
   end
   def new
+    @page_title = '新規作成' + @title
     @member = Member.new(birthday: Date.new(2000, 1, 1))
   end
   def edit
+    @page_title = '編集' + @title
     @member = Member.find(params[:id])
   end
   def create
